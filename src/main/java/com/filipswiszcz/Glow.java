84fd285747060ca41e898844af5ceb0a574e86c4
Filip Świszcz <65617364+filip-swiszcz@users.noreply.github.com>
2022-12-12 13:00:08 +0100

    Add some settings

--- src/main/java/com/filipswiszcz/Glow.java
+++ src/main/java/com/filipswiszcz/Glow.java
@@ -4,26 +4,47 @@ import java.util.logging.Logger;
 
 import com.filipswiszcz.world.Difficulty;
 
+/**
+ * Ideas:
+ * - player can ask glow a question on chat, which will be answered
+ * - npcs dialogues will be shown near them
+ * - server responses will be shown above of player's inventory bar
+ */
 public final class Glow {
 
-    private final Logger logger = Logger.getLogger("Glow");
+    private static final Logger logger = Logger.getLogger("Glow");
 
-    private final String version = "1.0-SNAPSHOT";
-    private final Difficulty difficulty = Difficulty.NORMAL;
+    private static final String version = "1.0-SNAPSHOT";
+    private static final int ticks = 1000 / 20;
+    private static final int chunkViewDistance = 8;
+    private static final int entityViewDistance = 5;
+    private static final Difficulty difficulty = Difficulty.NORMAL;
 
     public Glow() {
 
     }
 
-    public Logger getLogger() {
+    public static Logger getLogger() {
         return logger;
     }
 
-    public String getVersion() {
+    public static String getVersion() {
         return version;
     }
 
-    public Difficulty geDifficulty() {
+    public static int getTicks() {
+        return ticks;
+    }
+
+    public static int getChunkViewDistance() {
+        return chunkViewDistance;
+    }
+
+    public static int getEntityViewDistance() {
+        return entityViewDistance;
+    }
+
+    public static Difficulty geDifficulty() {
         return difficulty;
     }
 
