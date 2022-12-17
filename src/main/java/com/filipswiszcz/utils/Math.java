56d65194ac631aede8261a22143e4db9af75c057
Filip Świszcz <65617364+filip-swiszcz@users.noreply.github.com>
2022-12-17 02:26:44 +0100

    Add utils package

---
+++ src/main/java/com/filipswiszcz/utils/Math.java
@@ -0,0 +1,17 @@
+package com.filipswiszcz.utils;
+
+public final class Math {
+
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static float square(float number) {
+        return number * number;
+    }
+
+    public static double square(double number) {
+        return number * number;
+    }
+    
+}
